720acd7278fefc3da51cba8fdb693d0dac20b9d3
Pavadaran Pathmaranjan <pavadaran17@gmail.com>
2023-05-12 15:34:32 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@
 }; */
 
 const authConfig = {
-    /* signInRedirectURL: "https://b1737d06-83bf-4319-b114-afa517372815.e1-us-east-azure.choreoapps.dev",
-    signOutRedirectURL: "https://b1737d06-83bf-4319-b114-afa517372815.e1-us-east-azure.choreoapps.dev", */
-    signInRedirectURL: "https://localhost:3000",
-    signOutRedirectURL: "https://localhost:3000",
+    signInRedirectURL: "https://b1737d06-83bf-4319-b114-afa517372815.e1-us-east-azure.choreoapps.dev",
+    signOutRedirectURL: "https://b1737d06-83bf-4319-b114-afa517372815.e1-us-east-azure.choreoapps.dev",
+    /* signInRedirectURL: "https://localhost:3000",
+    signOutRedirectURL: "https://localhost:3000", */
     clientID: "sH9eFAfctbq7hfrgvt4rwtej8Vsa",
     baseUrl: "https://api.asgardeo.io/t/gramainc",
     scope: ['openid', 'profile'],
