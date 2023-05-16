36374b534e313d1ef1362ee5caff3a65275997c7
Pavadaran Pathmaranjan <pavadaran17@gmail.com>
2023-05-16 10:18:41 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
-const authConfig = {
+/*const authConfig = {
     signInRedirectURL: `${process.env.WEB_URL}`,
     signOutRedirectURL: `${process.env.WEB_URL}`,
     clientID: process.env.CLIENT_ID,
     baseUrl: process.env.ASGARDEO_BASE_URL,
     scope: ['openid', 'profile'],
-};
+};*/
 
-/*const authConfig = {
+const authConfig = {
     // signInRedirectURL: "https://b1737d06-83bf-4319-b114-afa517372815.e1-us-east-azure.choreoapps.dev",
     // signOutRedirectURL: "https://b1737d06-83bf-4319-b114-afa517372815.e1-us-east-azure.choreoapps.dev",
      signInRedirectURL: "https://localhost:3000",
@@ -14,6 +14,6 @@ const authConfig = {
     clientID: "sH9eFAfctbq7hfrgvt4rwtej8Vsa",
     baseUrl: "https://api.asgardeo.io/t/gramainc",
     scope: ['openid', 'profile'],
-};*/
+};
 
 export default authConfig;
