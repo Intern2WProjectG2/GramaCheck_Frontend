32ff105df0ba781c1188d434608ab19a94b70545
NELMIEK99 <nelmikudagodage@gmail.com>
2023-05-16 15:42:24 +0530

    Update profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect} from "react";
+import { useLocation,Link} from "react-router-dom";
 import { LogoutRequestDenied } from "./LogoutRequestDenied.jsx";
 import { useAuthContext } from "@asgardeo/auth-react";
 import LOGO from '../images/logo.png';
@@ -37,12 +37,16 @@ export const Header = ({setHasLogoutFailureError, hasLogoutFailureError}) => {
         );
     }
 
+    
     return (
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <h1 className="navbar-brand" href="#">
+                    <Link to="/">  
                     <img src={LOGO} alt="" width="200" height="35" className="d-inline-block align-text-top" />
+                    </Link>
+                  
 
                 </h1>
                 <button
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../images/profile.jpg";
+import profile from "../images/profile.png";
 import "../App.css";
 
 export const Profile = () => {
@@ -7,9 +7,9 @@ export const Profile = () => {
   return (
     <div class="container justify-content-center"  style={{border:"#fff"}}>
     <div class="row border-curve" style={{border:"1px solid #0c255b"}}>
-      <div class="col-lg-4 left-aligned"style={{border:"#fff"}}>
-        <div class="card justify-content-center"style={{border:"#fff"}}>
-           <div class="card-body justify-content-center"style={{border:"#fff"}}>
+      <div class="col-lg-5 "style={{border:"#fff"}}>
+        <div class="card text-center"style={{border:"#fff"}}>
+           <div class="card-body justify-content-center"style={{border:"#fff", alignItems:"center"}}>
            <img src={profile} class="card-img-top"  style={{width:"200px", height:"200px", alignItems:"center"}} alt="Card 1 Image"/>
            </div>
           </div>
@@ -17,30 +17,30 @@ export const Profile = () => {
         <div class="col-lg-6 left-aligned"style={{border:"#fff"}}>
         <div class="card "style={{border:"#fff"}}>
            <div class="card-body ">
-            {/* <h4>NIC :  <span style={{fontSize:"20px"}}>987635567V</span> <br/></h4>
-            <h4>ADDRESS :  <span style={{fontSize:"20px"}}>89/1A, </span> 
-            <span style={{fontSize:"20px"}}>Nugegoda, </span> 
-            <span style={{fontSize:"20px"}}>Colombo, </span>
-            <span style={{fontSize:"20px"}}>Western Province</span> <br/></h4>
-            <h4>POSTAL CODE :  <span style={{fontSize:"20px"}}>99067</span> <br/></h4>
-            <h4>GRAMASEWA DIVISION NUMBER :  <span style={{fontSize:"20px"}}>4567</span> <br/></h4> */}
             <div class="table-responsive">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-          </tr>
-        </thead>
+      <table class="table table-borderless align-middle">
+     
         <tbody>
           <tr>
-            <td>1</td>
-            <td>John Doe</td>
-            <td>john@example.com</td>
-            <td>Admin</td>
+          <th scope="row">NIC</th>
+          <td>889756789V</td>
           </tr>
+
+          <tr>
+          <th scope="row">ADDRESS</th>
+          <td>67/9S, Nugegoda, Colombo, Western Province</td>
+          </tr>
+
+          <tr>
+          <th scope="row">POSTAL CODE</th>
+          <td>11234</td>
+          </tr>
+
+          <tr>
+          <th scope="row">GRAMASEWA DIVISION NUMBER</th>
+          <td>12345</td>
+          </tr>
+
         </tbody>
       </table>
     </div>
