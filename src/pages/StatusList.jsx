6626d5c30b175a388838d11a53b0813ed3759ff6
Rashini1998 <gamalathrashini@gmail.com>
2023-05-17 15:11:32 +0530

    add help page

--- src/pages/StatusList.jsx
+++ src/pages/StatusList.jsx
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import {Header} from "../components/Header.jsx";
+import { Header } from "../components/Header.jsx";
+import { useAuthContext } from "@asgardeo/auth-react";
+import { getAllApps } from "../services/apiClient";
 
 export const StatusList = () => {
     const history = useHistory();
+    const [apps, setApps] = useState([]);
+
+    const {
+        getAccessToken
+      } = useAuthContext();
 
     const data = [
         { date: "2023 May 04", nic: "923467889v", address: "No 123, Galle Road, Colombo 03" },
         { date: "2023 April 09", nic: "923467889v", address: "No 145, Galle Road, Colombo 03" },
-        { date: "2023 January 01", nic: "923467889v" , address: "No 13, Galle Road, Colombo 03"},
+        { date: "2023 January 01", nic: "923467889v", address: "No 13, Galle Road, Colombo 03" },
     ];
 
+    const getApps = async () => {
+        try {
+            const allApps = await getAllApps(await getAccessToken());
+            setApps(allApps.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const handleCheckStatus = (item) => {
-        history.push(`/check-status?date=${encodeURIComponent(item.date)}&nic=${encodeURIComponent(item.nic)}&address=${encodeURIComponent(item.address)}`);
+        history.push(`/check-status?date=${encodeURIComponent(item.issueDate)}&nic=${encodeURIComponent(item.inputNIC)}&address=${encodeURIComponent(item.inputAddress)}`);
     };
+
+    useEffect(() => {
+        getApps();
+    }, []);
+
+    useEffect(() => {
+        console.log(apps);
+    }, [apps]);
 
     return (
         <>
-        <Header />
-            <h1 style={{ fontSize:"40px", marginTop:"25px", color:"#282c34"}}>Applications</h1>
-            <div style={{ margin: "20px", marginTop:"40px" }}>
-                <ul class="list-group">
-                    {data.map((item, index) => (
-                        <li href="#" class="list-group-item d-flex justify-content-between align-items-center">
-                            {item.date}
+            <Header />
+            <h1 style={{ fontSize: "40px", marginTop: "25px", color: "#282c34" }}>Applications</h1>
+            {apps && <div style={{ margin: "20px", marginTop: "40px" }}>
+                <ul className="list-group">
+                    {apps.map((item, index) => (
+                        <li href="#" className="list-group-item d-flex justify-content-between align-items-center">
+                            {item.issueDate}
                             <button
                                 type="button"
                                 className="btn btn-primary btn-sm"
@@ -47,7 +71,7 @@ export const StatusList = () => {
 
 
                 </ul>
-            </div>
+            </div>}
         </>
     );
 };
